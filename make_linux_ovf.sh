794a6669e2b41e10a216cd20ce3f14140a16fa35
Matt Lindahl <mlindahl@skytap.com>
2020-01-07 10:59:49 -0800

    Switch ovf creation to use heredoc redirects

--- make_linux_ovf.sh
+++ make_linux_ovf.sh
@@ -69,92 +69,119 @@ done
 
 echo ""
 echo 'Creating OVF file: '$LPAR_NAME'.ovf'
-> $LPAR_NAME'.ovf'
-echo '<?xml version="1.0" encoding="UTF-8"?>' >> $LPAR_NAME'.ovf'
-echo '<ovf:Envelope xmlns:ovf="http://schemas.dmtf.org/ovf/envelope/1" xmlns:vssd="http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2/CIM_VirtualSystemSettingData" xmlns:rasd="http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2/CIM_ResourceAllocationSettingData" xmlns:skytap="http://help.skytap.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2/CIM_VirtualSystemSettingData http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2.22.0/CIM_VirtualSystemSettingData.xsd http://schemas.dmtf.org/ovf/envelope/1 http://schemas.dmtf.org/ovf/envelope/1/dsp8023_1.1.0.xsd http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2/CIM_ResourceAllocationSettingData http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2.22.0/CIM_ResourceAllocationSettingData.xsd">' >> $LPAR_NAME'.ovf'
-echo '    <ovf:References>' >> $LPAR_NAME'.ovf'
+cat << EOF > $LPAR_NAME.ovf
+<?xml version="1.0" encoding="UTF-8"?>
+<ovf:Envelope xmlns:ovf="http://schemas.dmtf.org/ovf/envelope/1" xmlns:vssd="http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2/CIM_VirtualSystemSettingData" xmlns:rasd="http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2/CIM_ResourceAllocationSettingData" xmlns:skytap="http://help.skytap.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2/CIM_VirtualSystemSettingData http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2.22.0/CIM_VirtualSystemSettingData.xsd http://schemas.dmtf.org/ovf/envelope/1 http://schemas.dmtf.org/ovf/envelope/1/dsp8023_1.1.0.xsd http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2/CIM_ResourceAllocationSettingData http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2.22.0/CIM_ResourceAllocationSettingData.xsd">
+    <ovf:References>
+EOF
 for arg in "$@";do
-   echo '        <ovf:File ovf:id="file_'$arg'" ovf:href="'$LPAR_NAME-$arg'.img"/>' >> $LPAR_NAME'.ovf'
+   cat << EOF >> $LPAR_NAME.ovf
+        <ovf:File ovf:id="file_$arg" ovf:href="$LPAR_NAME-$arg.img"/>
+EOF
 done
-echo '    </ovf:References>' >> $LPAR_NAME'.ovf'
-echo '    <ovf:DiskSection>' >> $LPAR_NAME'.ovf'
-echo '        <ovf:Info>Virtual disk information</ovf:Info>' >> $LPAR_NAME'.ovf'
+cat << EOF >> $LPAR_NAME.ovf
+    </ovf:References>
+    <ovf:DiskSection>
+        <ovf:Info>Virtual disk information</ovf:Info>
+EOF
 for arg in "$@";do
    DISK_ALLOCATION=$(lsblk --output SIZE -n -d -b /dev/$arg)
-   echo '        <ovf:Disk ovf:fileRef="file_'$arg'" ovf:diskId="disk_'$arg'" ovf:capacity="'$DISK_ALLOCATION'"/>' >> $LPAR_NAME'.ovf'
+   cat << EOF >> $LPAR_NAME.ovf
+            <ovf:Disk ovf:fileRef="file_$arg" ovf:diskId="disk_$arg" ovf:capacity="$DISK_ALLOCATION"/>
+EOF
 done
-echo '    </ovf:DiskSection>' >> $LPAR_NAME'.ovf'
-echo '    <ovf:VirtualSystemCollection ovf:id="Linux">' >> $LPAR_NAME'.ovf'
-echo '        <ovf:VirtualSystem ovf:id="Linux MachineName">' >> $LPAR_NAME'.ovf'
-echo '            <ovf:Name>'$LPAR_NAME'</ovf:Name>' >> $LPAR_NAME'.ovf'
-echo '            <ovf:OperatingSystemSection ovf:id="80">' >> $LPAR_NAME'.ovf'
-echo '                <ovf:Info/>' >> $LPAR_NAME'.ovf'
-echo '                <ovf:Description>Linux on Power</ovf:Description>' >> $LPAR_NAME'.ovf'
-echo '                <ns0:architecture xmlns:ns0="ibmpvc">ppc64</ns0:architecture>' >> $LPAR_NAME'.ovf'
-echo '            </ovf:OperatingSystemSection>' >> $LPAR_NAME'.ovf'
-echo '            <ovf:VirtualHardwareSection>' >> $LPAR_NAME'.ovf' >> $LPAR_NAME'.ovf'
+   cat << EOF >> $LPAR_NAME.ovf
+    </ovf:DiskSection>
+    <ovf:VirtualSystemCollection ovf:id="Linux">
+        <ovf:VirtualSystem ovf:id="Linux MachineName">
+            <ovf:Name>$LPAR_NAME</ovf:Name>
+            <ovf:OperatingSystemSection ovf:id="80">
+                <ovf:Info/>
+                <ovf:Description>Linux on Power</ovf:Description>
+                <ns0:architecture xmlns:ns0="ibmpvc">ppc64</ns0:architecture>
+            </ovf:OperatingSystemSection>
+            <ovf:VirtualHardwareSection>
+EOF
 COUNT=1
 for e in $ETHERNET_ADAPTERS;do
    SLOT=$(lscfg -l $e | sed -n 's/.*-C\([^-]*\)-.*/\1/p')
    NETCIDR=$(ip -4 addr show $e | grep inet | awk '{print $2}')
    NETADDR=$(echo $NETCIDR | awk -F/ '{print $1}')
    NETMASK=$(ipcalc -m $NETCIDR | awk -F= '{ print $2 }')
-   echo '                <ovf:Item>' >> $LPAR_NAME'.ovf'
-   echo '                    <rasd:Description>Ethernet adapter '$COUNT'</rasd:Description>' >> $LPAR_NAME'.ovf'
-   echo '                    <rasd:ElementName>Network adapter '$COUNT'</rasd:ElementName>' >> $LPAR_NAME'.ovf'
-   echo '                    <rasd:InstanceID>10'$COUNT'</rasd:InstanceID>' >> $LPAR_NAME'.ovf'
-   echo '                    <rasd:ResourceType>10</rasd:ResourceType>' >> $LPAR_NAME'.ovf'
+   cat << EOF >> $LPAR_NAME.ovf
+                <ovf:Item>
+                    <rasd:Description>Ethernet adapter $COUNT</rasd:Description>
+                    <rasd:ElementName>Network adapter $COUNT</rasd:ElementName>
+                    <rasd:InstanceID>10$COUNT</rasd:InstanceID>
+                    <rasd:ResourceType>10</rasd:ResourceType>
+EOF
    if [ -n "$SLOT" ]; then
-      echo '                    <skytap:Config skytap:value="'$SLOT'" skytap:key="slotInfo.cardSlotNumber"/>' >> $LPAR_NAME'.ovf'
+      cat << EOF >> $LPAR_NAME.ovf
+                    <skytap:Config skytap:value="$SLOT" skytap:key="slotInfo.cardSlotNumber"/>
+EOF
    fi
    if [ -n "$NETADDR" ]; then
-      echo '                    <skytap:Config skytap:value="'$NETADDR'" skytap:key="networkInterface.ipAddress"/>' >> $LPAR_NAME'.ovf'
+      cat << EOF >> $LPAR_NAME.ovf
+                    <skytap:Config skytap:value="$NETADDR" skytap:key="networkInterface.ipAddress"/>
+EOF
    fi
    if [ -n "$NETMASK" ]; then
-      echo '                    <skytap:Config skytap:value="'$NETMASK'" skytap:key="networkInterface.netmask"/>' >> $LPAR_NAME'.ovf'
+      cat << EOF >> $LPAR_NAME.ovf
+                    <skytap:Config skytap:value="$NETMASK" skytap:key="networkInterface.netmask"/>
+EOF
    fi
-   echo '                </ovf:Item>' >> $LPAR_NAME'.ovf'
+   cat << EOF >> $LPAR_NAME.ovf
+                </ovf:Item>
+EOF
    ((COUNT=COUNT+1))
 done
 COUNT=1
 for arg in "$@";do
    SLOT=$(lscfg -l $arg | sed -n 's/.*-C\([^-]*\)-.*/\1/p')
-   echo '                <ovf:Item>' >> $LPAR_NAME'.ovf'
-   echo '                    <rasd:Description>Hard disk</rasd:Description>' >> $LPAR_NAME'.ovf'
-   echo '                    <rasd:ElementName>Hard disk '$COUNT'</rasd:ElementName>' >> $LPAR_NAME'.ovf'
-   echo '                    <rasd:HostResource>ovf:/disk/disk_'$arg'</rasd:HostResource>' >> $LPAR_NAME'.ovf'
-   echo '                    <rasd:InstanceID>100'$COUNT'</rasd:InstanceID>' >> $LPAR_NAME'.ovf'
-   echo '                    <rasd:ResourceType>17</rasd:ResourceType>' >> $LPAR_NAME'.ovf'
+   cat << EOF >> $LPAR_NAME.ovf
+                <ovf:Item>
+                    <rasd:Description>Hard disk</rasd:Description>
+                    <rasd:ElementName>Hard disk $COUNT</rasd:ElementName>
+                    <rasd:HostResource>ovf:/disk/disk_$arg</rasd:HostResource>
+                    <rasd:InstanceID>100$COUNT</rasd:InstanceID>
+                    <rasd:ResourceType>17</rasd:ResourceType>
+EOF
    if [ -n "$SLOT" ]; then
-      echo '                    <skytap:Config skytap:value="'$SLOT'" skytap:key="slotInfo.cardSlotNumber"/>' >> $LPAR_NAME'.ovf'
+         cat << EOF >> $LPAR_NAME.ovf
+                    <skytap:Config skytap:value="$SLOT" skytap:key="slotInfo.cardSlotNumber"/>
+EOF
    fi
-   echo '                </ovf:Item>' >> $LPAR_NAME'.ovf'
+   cat << EOF >> $LPAR_NAME.ovf
+                </ovf:Item>
+EOF
    ((COUNT=COUNT+1))
 done
-echo '                <ovf:Item>' >> $LPAR_NAME'.ovf'
-echo '                    <rasd:AllocationUnits>hertz * 10^6</rasd:AllocationUnits>' >> $LPAR_NAME'.ovf'
-echo '                    <rasd:Description>Number of Virtual CPUs</rasd:Description>' >> $LPAR_NAME'.ovf'
-echo '                    <rasd:ElementName>'$VIRTUAL_PROCESSORS' virtual CPU(s)</rasd:ElementName>' >> $LPAR_NAME'.ovf'
-echo '                    <rasd:InstanceID>7</rasd:InstanceID>' >> $LPAR_NAME'.ovf'
-echo '                    <rasd:Reservation>0</rasd:Reservation>' >> $LPAR_NAME'.ovf'
-echo '                    <rasd:ResourceType>3</rasd:ResourceType>' >> $LPAR_NAME'.ovf'
-echo '                    <rasd:VirtualQuantity>'$VIRTUAL_PROCESSORS'</rasd:VirtualQuantity>' >> $LPAR_NAME'.ovf'
-echo '                    <rasd:Weight>0</rasd:Weight>' >> $LPAR_NAME'.ovf'
-echo '                </ovf:Item>' >> $LPAR_NAME'.ovf'
-echo '                <ovf:Item>' >> $LPAR_NAME'.ovf'
-echo '                    <rasd:AllocationUnits>byte * 2^20</rasd:AllocationUnits>' >> $LPAR_NAME'.ovf'
-echo '                    <rasd:Description>Memory Size</rasd:Description>' >> $LPAR_NAME'.ovf'
-echo '                    <rasd:ElementName>'$RAM_ALLOCATION' MB of memory</rasd:ElementName>' >> $LPAR_NAME'.ovf'
-echo '                    <rasd:InstanceID>8</rasd:InstanceID>' >> $LPAR_NAME'.ovf'
-echo '                    <rasd:Reservation>0</rasd:Reservation>' >> $LPAR_NAME'.ovf'
-echo '                    <rasd:ResourceType>4</rasd:ResourceType>' >> $LPAR_NAME'.ovf'
-echo '                    <rasd:VirtualQuantity>'$RAM_ALLOCATION'</rasd:VirtualQuantity>' >> $LPAR_NAME'.ovf'
-echo '                    <rasd:Weight>0</rasd:Weight>' >> $LPAR_NAME'.ovf'
-echo '                </ovf:Item>' >> $LPAR_NAME'.ovf'
-echo '            </ovf:VirtualHardwareSection>' >> $LPAR_NAME'.ovf'
-echo '        </ovf:VirtualSystem>' >> $LPAR_NAME'.ovf'
-echo '    </ovf:VirtualSystemCollection>' >> $LPAR_NAME'.ovf'
-echo '</ovf:Envelope>' >> $LPAR_NAME'.ovf'
+cat << EOF >> $LPAR_NAME.ovf
+                <ovf:Item>
+                    <rasd:AllocationUnits>hertz * 10^6</rasd:AllocationUnits>
+                    <rasd:Description>Number of Virtual CPUs</rasd:Description>
+                    <rasd:ElementName>$VIRTUAL_PROCESSORS virtual CPU(s)</rasd:ElementName>
+                    <rasd:InstanceID>7</rasd:InstanceID>
+                    <rasd:Reservation>0</rasd:Reservation>
+                    <rasd:ResourceType>3</rasd:ResourceType>
+                    <rasd:VirtualQuantity>$VIRTUAL_PROCESSORS</rasd:VirtualQuantity>
+                    <rasd:Weight>0</rasd:Weight>
+                </ovf:Item>
+                <ovf:Item>
+                    <rasd:AllocationUnits>byte * 2^20</rasd:AllocationUnits>
+                    <rasd:Description>Memory Size</rasd:Description>
+                    <rasd:ElementName>$RAM_ALLOCATION MB of memory</rasd:ElementName>
+                    <rasd:InstanceID>8</rasd:InstanceID>
+                    <rasd:Reservation>0</rasd:Reservation>
+                    <rasd:ResourceType>4</rasd:ResourceType>
+                    <rasd:VirtualQuantity>$RAM_ALLOCATION</rasd:VirtualQuantity>
+                    <rasd:Weight>0</rasd:Weight>
+                </ovf:Item>
+            </ovf:VirtualHardwareSection>
+        </ovf:VirtualSystem>
+    </ovf:VirtualSystemCollection>
+</ovf:Envelope>
+EOF
 
 ## OVF completed
 echo 'OVF Completed Successfully'
